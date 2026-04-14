da1194687219aa83e024135135ab9c33b9a6975d
小唉LEntropy <Ai_LEntropy@126.com>
2026-04-14 10:42:33 +0800

    Complete exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -9,6 +9,11 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    
     const newPerson = {
       name: newName,
       id: persons.length + 1
